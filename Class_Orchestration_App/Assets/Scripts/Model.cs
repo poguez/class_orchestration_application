40474266cfdc9bbb547314ce4f9176c07bf9df65
Lou <lou.kramer@tum.de>
2017-05-09 15:22:11 +0900

    Add basic unity project. Load model in front of camera, transformations are done in object space. Controls: press numpad keys 0 to 4 for choosing model; press s + move mouse for scaling, press r + move mouse for rotation, press t + move mouse for translation. Add basic gamepad support for scaling, rotation and translation.

---
+++ Class_Orchestration_App/Assets/Scripts/Model.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model : MonoBehaviour {
+
+    public GameObject[] objects = new GameObject[5];
+
+    // TODO: retrieve ID from database
+    public int modelID;
+
+    private GameObject model;
+
+
+    private bool scale = false;
+    private bool rotate = false;
+    private bool translate = false;
+
+    public float rotSpeed = 100.0f;
+    public float transSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        model = objects[modelID];
+        model.transform.localPosition = new Vector3(0.0f, 0.0f, 10.0f);
+	}
+
+    // TODO: handle input
+
+    // Update is called once per frame
+    void Update() {
+        // TODO: if model selection is enabled
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            modelID = 0;
+            resetObject();
+        }
+            
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            modelID = 1;
+            resetObject();
+        }
+            
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            modelID = 2;
+            resetObject();
+        }
+            
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            modelID = 3;
+            resetObject();
+        }
+            
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            modelID = 4;
+            resetObject();
+        }
+            
+        // TODO: if interaction is enabled
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetButtonDown("Fire3"))
+            scale = true;
+
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetButtonDown("Fire3"))
+            scale = false;
+
+        if (scale)
+        {
+            {
+                model.transform.localScale = model.transform.localScale + new Vector3(1.0f, 1.0f, 1.0f) * Input.GetAxis("Mouse X") * Time.deltaTime;
+                model.transform.localScale = model.transform.localScale + new Vector3(1.0f, 1.0f, 1.0f) * Input.GetAxis("Horizontal") * Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) || Input.GetButtonDown("Fire1"))
+            rotate = true;
+
+        if (Input.GetKeyUp(KeyCode.R) || Input.GetButtonUp("Fire1"))
+            rotate = false;
+
+        if (rotate)
+        {
+            model.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), -1 * Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotSpeed);
+            model.transform.Rotate(new Vector3(Input.GetAxis("Vertical"), -1 * Input.GetAxis("Horizontal"), 0) * Time.deltaTime * rotSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T) || Input.GetButtonDown("Fire2")) {
+            translate = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.T) || Input.GetButtonDown("Fire2"))
+            translate = false;
+
+        if(translate)
+        {
+            model.transform.Translate((Vector3.up * Input.GetAxis("Mouse Y") + Vector3.right * Input.GetAxis("Mouse X")) * Time.deltaTime * transSpeed);
+            model.transform.Translate((Vector3.up * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal")) * Time.deltaTime * transSpeed);
+        }
+
+        // TODO: synchronize transformation with database
+
+        // TODO: retrieve transformation update from database
+        // scaling, rotation, translation
+    }
+
+    private void resetObject()
+    {
+        model.transform.localPosition = new Vector3(0.0f, 0.0f, -10.0f);
+        model = objects[modelID];
+        model.transform.localPosition = new Vector3(0.0f, 0.0f, 10.0f);
+    }
+}
