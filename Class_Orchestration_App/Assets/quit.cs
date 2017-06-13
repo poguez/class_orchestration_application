e07577982dfa05d2647f4cd5d6412bd9bbb1b075
Valentin Porcellini <valentin.porcellini@gmail.com>
2017-06-13 16:07:30 +0900

    Added back to menu / quit buttons + UI Fixes for model selection

---
+++ Class_Orchestration_App/Assets/quit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void quit_app(){
+
+		Application.Quit();
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
