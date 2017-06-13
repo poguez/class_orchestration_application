cb9d5f6cbeb3860137a15484d4f59b4f2786ce2f
poguez <zeugop@gmail.com>
2017-06-13 22:45:42 +0900

    Added support for user api: create, get all users

---
+++ Class_Orchestration_App/Assets/rest_client/UsersApi.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using ClassOrchestrationApi;
+using SimpleJSON;
+
+//
+// Unity Web Request
+//
+// https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.html
+
+public class UsersApi : MonoBehaviour {
+	ClassOrchestrationApi.RestClient myclient;
+	ClassOrchestrationApi.ActionWWW getUsersCallback = GetUsersCallback;
+	ClassOrchestrationApi.ActionWWW createUserCallback = CreateUserCallback;
+	string base_url = "http://miri.noedominguez.com:9000";
+	//string base_url = "http://localhost:9000";
+	static string all_users_json;
+	static User myUser = new User();
+
+	[Serializable]
+	public class User
+	{
+		public int user_id;
+		public string username;
+		public bool isAdmin;
+		public int teamId;
+
+		public string toString(){
+			return "user_id: " + user_id + ", " + "name: " + username + ", " + "isAdmin: " + isAdmin + ", " + "teamId: " + teamId;
+		}
+	}
+
+	void Start () {
+		myclient = (new GameObject("RESTclient")).AddComponent<RestClient>();
+
+		getUsers ().ToString ();
+		createUser ().ToString();
+	}
+	
+	void Update () {
+	}
+
+
+	// Get all the users
+	public string getUsers(){
+		string get_url = base_url + "/v1/user/";
+		UnityWebRequest reponse = myclient.GET(get_url, getUsersCallback);
+		return reponse.downloadHandler.text;
+	}
+
+	// Create a user
+	public string createUser(){
+		string put_url = base_url + "/v1/user/new";
+		string requestBodyJsonString = "{\"name\": \"Google Cardboard User\", \"password\": \"new\", \"isAdmin\": false, \"teamId\": 1 }";
+		UnityWebRequest reponse = myclient.POST(put_url, requestBodyJsonString, createUserCallback);
+		return reponse.downloadHandler.text;
+	}
+
+	// Log the UnityWebRequest with this callback
+	public static void CreateUserCallback(UnityWebRequest www)
+	{	
+		var result = JSON.Parse(www.downloadHandler.text);
+		myUser.user_id = result ["id"].AsInt;
+		myUser.username = result ["name"];
+		myUser.isAdmin = result ["isAdmin"];
+		myUser.teamId = result ["teamId"].AsInt;
+		Debug.Log ("User object print:");
+		Debug.Log ( myUser.toString() );
+	}
+	public static void GetUsersCallback(UnityWebRequest www)
+	{	
+		all_users_json = www.downloadHandler.text;
+		Debug.Log ("All users JSON response:");
+		Debug.Log ( all_users_json );
+	}
+
+
+
+}
