d2a1944af8e8fcea351d64fb048e333d5fa34b74
poguez <zeugop@gmail.com>
2017-06-14 03:21:32 +0900

    Added support for creating Exploration and ExplorationObjects

--- Class_Orchestration_App/Assets/rest_client/UsersApi.cs
+++ Class_Orchestration_App/Assets/rest_client/UsersApi.cs
@@ -15,11 +15,23 @@ public class UsersApi : MonoBehaviour {
 	ClassOrchestrationApi.RestClient myclient;
 	ClassOrchestrationApi.ActionWWW getUsersCallback = GetUsersCallback;
 	ClassOrchestrationApi.ActionWWW createUserCallback = CreateUserCallback;
+	ClassOrchestrationApi.ActionWWW createExplorationCallback = CreateExplorationCallback;
+	ClassOrchestrationApi.ActionWWW createExplorationObjectCallback = CreateExplorationObjectCallback;
 	string base_url = "http://miri.noedominguez.com:9000";
 	//string base_url = "http://localhost:9000";
 	static string all_users_json;
 	static User myUser = new User();
+	static Exploration myExploration = new Exploration();
+	static ExplorationObject myExplorationObject = new ExplorationObject();
 
+
+
+	/*
+	 * 
+	 * Classes to create objects that save state
+	 * 
+	 * 
+	 */
 	[Serializable]
 	public class User
 	{
@@ -33,19 +45,65 @@ public class UsersApi : MonoBehaviour {
 		}
 	}
 
+	[Serializable]
+	public class Exploration
+	{
+		public int explorationId;
+		public int teacherId;
+		public int explorationObjectId;
+		public string toString(){
+			return "id: "+ explorationId + ", " + "teacherId: " + teacherId + ", " + "explorationObjectId: " + explorationObjectId;
+		}
+	}
+
+	[Serializable]
+	public class ExplorationObject
+	{
+		public int explorationObjectId;
+		public string objectState;
+		public string modelName; // "dinosaur", "desk" or "dna"
+		public string toString(){
+			return "id: " + explorationObjectId + ", " + "objectState: " + objectState + "modelName: " + modelName;
+		}
+	}
+
+	/*
+	 * 
+	 * Default Monobehavior 
+	 * 
+	 * 
+	 */
+
+
+
 	void Start () {
+		//Assign the Rest client to an object to use ClassOrchestrationApi;
 		myclient = (new GameObject("RESTclient")).AddComponent<RestClient>();
 
 		// Examples
 		//getUsers ().ToString ();
-		createUser ().ToString();
-		createUser ("professor").ToString();
+		//createUser ().ToString(); // create a student
+		//createUser ("professor").ToString(); // create a professor
 		//changeUserTeam(20, 14);
+		//createExplorationObject ();
+		//createExploration (2,19);
 	}
 	
 	void Update () {
+		
 	}
 
+	/*
+	 * 
+	 * API calls to the server.
+	 * 
+	 * 
+	 */
+
+
+	/*
+	*	Users and Groups Api calls
+	*/
 
 	// Get all the users
 	public string getUsers(){
@@ -74,7 +132,58 @@ public class UsersApi : MonoBehaviour {
 		return reponse.downloadHandler.text;
 	}
 
-	// Log the UnityWebRequest with this callback
+	/*
+	* Explorations Api calls
+	*/
+
+	// Create a Exploration
+	public string createExploration(int teacherId = -1, int explorationObjectId = -1){
+		string teacherIdParam = teacherId.ToString();
+		string explorationObjectIdParam = explorationObjectId.ToString();
+		if (teacherId == -1)
+		{
+			teacherIdParam = "null";
+		}
+		if (explorationObjectId == -1)
+		{
+			explorationObjectIdParam = "null";
+		}
+
+		string put_url = base_url + "/v1/exploration/new";
+		string requestBodyJsonString = "{\"teacherId\":" + teacherIdParam + ", " + "\"explorationObjectId\" :" +  explorationObjectIdParam + "}";
+		UnityWebRequest reponse = myclient.POST(put_url, requestBodyJsonString, createExplorationCallback);
+		return reponse.downloadHandler.text;
+	}
+
+	/*
+	* 	ExplorationObjects Api calls
+	* 
+	*/
+
+	public string createExplorationObject(string objectState = "", string modelName = "dinosaur"){
+		// modelName: "dinosaur", "desk" or "dna"
+		string objectStateParam = objectState;
+		string modelNameParam = modelName;
+		string put_url = base_url + "/v1/exploration-object/new";
+		string requestBodyJsonString = "{\"objectState\": \"" + objectStateParam + "\" , " + "\"modelName\" : \"" +  modelNameParam + "\" }";
+		UnityWebRequest reponse = myclient.POST(put_url, requestBodyJsonString, createExplorationObjectCallback);
+		return reponse.downloadHandler.text;
+	}
+
+
+
+
+	/*
+	 * 
+	 * Callback functions to be executed when result is received from
+	 * the API server.
+	 *
+	 * Log the UnityWebRequest with these callback
+	 *
+	 *
+	 */
+
+
 	public static void CreateUserCallback(UnityWebRequest www)
 	{	
 		var result = JSON.Parse(www.downloadHandler.text);
@@ -85,6 +194,29 @@ public class UsersApi : MonoBehaviour {
 		Debug.Log ("User object print:");
 		Debug.Log ( myUser.toString() );
 	}
+
+	public static void CreateExplorationCallback(UnityWebRequest www)
+	{	
+		var result = JSON.Parse(www.downloadHandler.text);
+		myExploration.explorationId = result ["id"].AsInt;
+		myExploration.teacherId = result ["teacherId"].AsInt;
+		myExploration.explorationObjectId = result ["explorationObjectId"].AsInt;
+		Debug.Log ("Exploration object print:");
+		Debug.Log ( myExploration.toString() );
+	}
+
+	public static void CreateExplorationObjectCallback(UnityWebRequest www)
+	{	
+		var result = JSON.Parse(www.downloadHandler.text);
+		myExplorationObject.explorationObjectId = result ["id"];
+		myExplorationObject.objectState = result ["objectState"];
+		myExplorationObject.modelName = result ["modelName"];
+		Debug.Log ("ExplorationObject object print:");
+		Debug.Log ( myExplorationObject.toString() );
+	}
+
+
+
 	public static void GetUsersCallback(UnityWebRequest www)
 	{	
 		all_users_json = www.downloadHandler.text;
