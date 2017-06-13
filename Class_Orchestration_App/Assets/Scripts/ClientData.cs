9abc1f6f64a245896aa24e981a61bba8a02fefd4
Lou <lou.kramer@tum.de>
2017-06-13 12:54:41 +0900

    Continue finishing UI functionality.

---
+++ Class_Orchestration_App/Assets/Scripts/ClientData.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClientData : MonoBehaviour {
+
+    public int userID = -1; // default -1: has not been assigned an ID by the server yet
+    public int teamID = 1; // default 1: main team, everyone is in there at the beginning
+
+    /* 
+     * 
+        0: guided exploration
+        1: individual exploration
+        2: group exploration
+     * 
+     */
+    public int explorationType = 0;
+
+    public bool isAdmin = false;
+
+    public Text joinTeamID;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setTeacher()
+    {
+        isAdmin = true;
+    }
+
+    public void joinTeam()
+    {
+        int id = int.Parse(joinTeamID.text);
+        teamID = id;
+    }
+
+    public void setGuided()
+    {
+        explorationType = 0;
+    }
+
+    public void setIndividual()
+    {
+        explorationType = 1;
+    }
+
+    public void setGroup()
+    {
+        explorationType = 2;
+    }
+}
---
+++ Class_Orchestration_App/Assets/Scripts/IncrText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IncrText : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void increment()
+    {
+        int iText = int.Parse(text.text);
+        if (iText < 10)
+        {
+            ++iText;
+            text.text = iText.ToString();
+        }
+    }
+
+    public void decrement()
+    {
+        int iText = int.Parse(text.text);
+        if (iText > 1)
+        {
+            --iText;
+            text.text = iText.ToString();
+        }
+    }
+}
