308362d5a63fe16be290f781b9ba1347d26aa172
Lou <lou.kramer@tum.de>
2017-06-14 17:16:34 +0900

    Setup teacher initialization.

--- Class_Orchestration_App/Assets/rest_client/usersApi.cs
+++ Class_Orchestration_App/Assets/rest_client/usersApi.cs
@@ -14,94 +14,99 @@ using SimpleJSON;
 // https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.html
 
 public class UsersApi : MonoBehaviour {
-	ClassOrchestrationApi.RestClient myclient;
-	ClassOrchestrationApi.ActionWWW getUsersCallback = GetUsersCallback;
-	ClassOrchestrationApi.ActionWWW createUserCallback = CreateUserCallback;
-	ClassOrchestrationApi.ActionWWW createExplorationCallback = CreateExplorationCallback;
-	ClassOrchestrationApi.ActionWWW createExplorationObjectCallback = CreateExplorationObjectCallback;
-	ClassOrchestrationApi.ActionWWW createEventCallback = CreateEventCallback;
-	string base_url = "http://miri.noedominguez.com:9000";
-	//string base_url = "http://localhost:9000";
-	static string all_users_json;
-	static User myUser = new User();
-	static Exploration myExploration = new Exploration();
-	static ExplorationObject myExplorationObject = new ExplorationObject();
-	static ExplorationEventManager myExplorationEventManager = new ExplorationEventManager();
-	//static var myExplorationEvent;
+    ClassOrchestrationApi.RestClient myclient;
+    ClassOrchestrationApi.ActionWWW getUsersCallback = GetUsersCallback;
+    ClassOrchestrationApi.ActionWWW createUserCallback = CreateUserCallback;
+    ClassOrchestrationApi.ActionWWW createExplorationCallback = CreateExplorationCallback;
+    ClassOrchestrationApi.ActionWWW createExplorationObjectCallback = CreateExplorationObjectCallback;
+    ClassOrchestrationApi.ActionWWW createEventCallback = CreateEventCallback;
+    string base_url = "http://miri.noedominguez.com:9000";
+    //string base_url = "http://localhost:9000";
+    static string all_users_json;
+    static User myUser = new User();
+    static Exploration myExploration = new Exploration();
+    static ExplorationObject myExplorationObject = new ExplorationObject();
+    static ExplorationEventManager myExplorationEventManager = new ExplorationEventManager();
+    //static var myExplorationEvent;
 
-	public Text teamID;
-	public GameObject model;
+    bool exploration = false;
+    bool teacherExplorationObject = false;
+    int oldObjectId = -1;
+    int currentObjectId;
+
+    public Text teamID;
+    public GameObject model;
 
     public GameObject explorationModeInfo;
 
-	/*
+    /*
 	 * 
 	 * Classes to create objects that save state
 	 * 
 	 * 
 	 */
-	[Serializable]
-	public class User
-	{
-		public int user_id;
-		public string username;
-		public bool isAdmin;
-		public int teamId;
+    [Serializable]
+    public class User
+    {
+        public int user_id;
+        public string username;
+        public bool isAdmin;
+        public int teamId;
 
-		public string toString(){
-			return "user_id: " + user_id + ", " + "name: " + username + ", " + "isAdmin: " + 
-				isAdmin + ", " + "teamId: " + teamId;
-		}
-	}
+        public string toString() {
+            return "user_id: " + user_id + ", " + "name: " + username + ", " + "isAdmin: " +
+                isAdmin + ", " + "teamId: " + teamId;
+        }
+    }
 
-	[Serializable]
-	public class Exploration
-	{
-		public int explorationId;
-		public int teacherId;
-		public int explorationObjectId;
-		public string toString(){
-			return "id: "+ explorationId + ", " + "teacherId: " + teacherId + ", " + 
-				"explorationObjectId: " + explorationObjectId;
-		}
-	}
+    [Serializable]
+    public class Exploration
+    {
+        public int explorationId;
+        public int teacherId;
+        public int explorationObjectId;
+        public string toString() {
+            return "id: " + explorationId + ", " + "teacherId: " + teacherId + ", " +
+                "explorationObjectId: " + explorationObjectId;
+        }
+    }
 
-	[Serializable]
-	public class ExplorationObject
-	{
-		public int explorationObjectId;
-		public string objectState;
-		public string modelName; // "dinosaur", "desk" or "dna"
-		public string toString(){
-			return "id: \"" + explorationObjectId + "\", \"" + "objectState\": \"" + objectState + 
-				"\" \"modelName\": \"" + modelName + "\"";
-		}
-	}
+    [Serializable]
+    public class ExplorationObject
+    {
+        public int explorationObjectId = -1;
+        public string objectState;
+        public string modelName; // "dinosaur", "desk" or "dna"
+        public string toString() {
+            return "id: \"" + explorationObjectId + "\", \"" + "objectState\": \"" + objectState +
+                "\" \"modelName\": \"" + modelName + "\"";
+        }
+    }
 
-	public class ExplorationEventManager
-	{
-		public int newestEventId;
-		public int lastProcessedEventId;
-		public Queue <UsersApi.ExplorationEvent> awaitingProcessEventsQueue = new Queue <UsersApi.ExplorationEvent>();
-		public void insertToQueue(UsersApi.ExplorationEvent eventForQueue){
-			myExplorationEventManager.awaitingProcessEventsQueue.Enqueue (eventForQueue);
-		}
+    public class ExplorationEventManager
+    {
+        public int newestEventId;
+        public int lastProcessedEventId;
+        public Queue<UsersApi.ExplorationEvent> awaitingProcessEventsQueue = new Queue<UsersApi.ExplorationEvent>();
+        public void insertToQueue(UsersApi.ExplorationEvent eventForQueue) {
+            myExplorationEventManager.awaitingProcessEventsQueue.Enqueue(eventForQueue);
+        }
 
-	}
-	public class ExplorationEvent
-	{
-		public int explorationEventId;
-		public string name;
-		public string description;
-		public string toString(){
-			return "id: \"" + explorationEventId + "\", " + "name: \"" + name + 
-				"\" description: \"" + description + "\"";
-		}
-	}
-	
+    }
+    public class ExplorationEvent
+    {
+        public int explorationEventId;
+        public string name;
+        public string description;
+        public string toString() {
+            return "id: \"" + explorationEventId + "\", " + "name: \"" + name +
+                "\" description: \"" + description + "\"";
+        }
+    }
 
 
-	/*
+
+    /*
 	 * 
 	 * Default Monobehavior 
 	 * 
@@ -110,22 +115,39 @@ public class UsersApi : MonoBehaviour {
 
 
 
-	void Start () {
-		//Assign the Rest client to an object to use ClassOrchestrationApi;
-		myclient = (new GameObject("RESTclient")).AddComponent<RestClient>();
+    void Start() {
+        //Assign the Rest client to an object to use ClassOrchestrationApi;
+        myclient = (new GameObject("RESTclient")).AddComponent<RestClient>();
 
-		// Examples
-		//getUsers ().ToString ();
-		//createUser (); // create a student
-		//createUser ("professor").ToString(); // create a professor
-		//changeUserTeam(20, 14);
-		//createExplorationObject ();
-		//createExploration (2,19);
-		createExplorationEvent ("changeExplorationObject", "This is a dummy description");
-	}
+        // Examples
+        //getUsers ().ToString ();
+        //createUser (); // create a student
+        //createUser ("professor").ToString(); // create a professor
+        //changeUserTeam(20, 14);
+        //createExplorationObject ();
+        //createExploration (2,19);
+        //createExplorationEvent ("changeExplorationObject", "This is a dummy description");
+    }
 
-	void Update () {
+    void Update() {
+        currentObjectId = myExplorationObject.explorationObjectId;
 
+        if (myUser.isAdmin && !exploration)
+        {
+            createExploration(myUser.user_id);
+            exploration = true;
+        }
+
+        if (myExplorationObject.explorationObjectId != -1)
+        {
+            teacherExplorationObject = true;
+        }
+
+        if (exploration && teacherExplorationObject && (oldObjectId != currentObjectId))
+        {
+            oldObjectId = currentObjectId;
+            updateExplorationObject(myExploration.explorationId, myExplorationObject.explorationObjectId);
+        }
 	}
 
     /*
@@ -144,14 +166,19 @@ public class UsersApi : MonoBehaviour {
     public void setExplorationMode(string mode)
     {
         explorationModeInfo.GetComponentInChildren<Text>().text = mode;
+        // guided
         if (mode.Equals("Guided")) {
             explorationModeInfo.GetComponent<Image>().color =
                 new Color32(0, 100, 255, 100);
-        } else if (mode.Equals("Indiv."))
+        }
+        // individual
+        else if (mode.Equals("Indiv."))
         {
             explorationModeInfo.GetComponent<Image>().color =
                 new Color32(255, 0, 100, 100);
-        } else
+        }
+        // group
+        else
         {
             explorationModeInfo.GetComponent<Image>().color =
                 new Color32(100, 255, 0, 100);
@@ -165,8 +192,9 @@ public class UsersApi : MonoBehaviour {
 		return reponse.downloadHandler.text;
 	}
 
-	// Create a user (userType: "professor" or "student by default")
-	public void createUser(string userType = "student"){
+
+    // Create a user (userType: "professor" or "student by default")
+    public void createUser(string userType = "student"){
 		string isAdmin = "false";
 		if (userType == "professor") {
 			isAdmin = "true";
@@ -175,7 +203,8 @@ public class UsersApi : MonoBehaviour {
 		string requestBodyJsonString = 
 			"{\"name\": \"Google Cardboard User\", \"password\": \"new\", \"isAdmin\":" + 
 			isAdmin + ", \"teamId\": 1 }";
-		UnityWebRequest reponse = myclient.POST(put_url, requestBodyJsonString, createUserCallback);
+        UnityWebRequest reponse = myclient.POST(put_url, requestBodyJsonString, createUserCallback);
+
 		return;// reponse.downloadHandler.text;
 	}
 
@@ -194,10 +223,11 @@ public class UsersApi : MonoBehaviour {
 
 	// Create a Exploration
 	// TEACHER
-	public string createExploration(int teacherId = -1, int explorationObjectId = -1){
-		string teacherIdParam = teacherId.ToString();
+	public void createExploration(int teacherId, int explorationObjectId = -1){
+        int teacher = teacherId;
+        string teacherIdParam = teacher.ToString();
 		string explorationObjectIdParam = explorationObjectId.ToString();
-		if (teacherId == -1)
+		if (teacher == -1)
 		{
 			teacherIdParam = "null";
 		}
@@ -209,17 +239,25 @@ public class UsersApi : MonoBehaviour {
 		string put_url = base_url + "/v1/exploration/new";
 		string requestBodyJsonString = "{\"teacherId\":" + teacherIdParam + ", " + 
 			"\"explorationObjectId\" :" +  explorationObjectIdParam + "}";
-		UnityWebRequest reponse = myclient.POST(put_url, requestBodyJsonString, createExplorationCallback);
-		return reponse.downloadHandler.text;
+        UnityWebRequest reponse = myclient.POST(put_url, requestBodyJsonString, createExplorationCallback);
+        return;// reponse.downloadHandler.text;
 	}
 
-	/*
+    public string updateExplorationObject(int explorationId, int explorationObjectId)
+    {
+        string put_url = base_url + "/v1/exploration/" + explorationId;
+        string requestBodyJsonString = "{\"explorationObjectId\" :" + explorationObjectId + "}";
+        UnityWebRequest reponse = myclient.POST(put_url, requestBodyJsonString, createExplorationCallback);
+        return reponse.downloadHandler.text;
+    }
+
+    /*
 	* 	ExplorationObjects Api calls
 	* 
 	*/
 
-	// TEACHER
-	public void createExplorationObject(string objectState = ""){
+    // TEACHER
+    public void createExplorationObject(string objectState = ""){
 		int id = model.GetComponent<Model>().modelID;
 
 		// modelName: "dinosaur", "desk" or "dna"
@@ -244,7 +282,7 @@ public class UsersApi : MonoBehaviour {
 		string requestBodyJsonString = "{\"objectState\": \"" + objectStateParam + "\" , " + 
 			"\"modelName\" : \"" +  modelNameParam + "\" }";
 		UnityWebRequest reponse = myclient.POST(put_url, requestBodyJsonString, createExplorationObjectCallback);
-		return;// reponse.downloadHandler.text;
+        return;// reponse.downloadHandler.text;
 	}
 
 	/*
@@ -256,8 +294,6 @@ public class UsersApi : MonoBehaviour {
 		string put_url = base_url + "/v1/exploration-event/new";
 		string requestBodyJsonString = 
 			"{\"name\": \"" + name + "\", " + "\"description\": \"" + description+"\" }";
-		Debug.Log ("This is the requestBody");
-		Debug.Log (requestBodyJsonString);
 		UnityWebRequest reponse = myclient.POST(put_url, requestBodyJsonString, createEventCallback);
 		return;// reponse.downloadHandler.text;
 	}
@@ -293,7 +329,7 @@ public class UsersApi : MonoBehaviour {
 		myExploration.explorationObjectId = result ["explorationObjectId"].AsInt;
 		Debug.Log ("Exploration object print:");
 		Debug.Log ( myExploration.toString() );
-	}
+    }
 
 	public static void CreateExplorationObjectCallback(UnityWebRequest www)
 	{	
